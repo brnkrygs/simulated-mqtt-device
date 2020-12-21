a71c064bd780c8f18e87c3633dff30e428e9694e
Brian Krygsman <brian.krygsman@gmail.com>
2020-12-21 16:39:30 -0500

    Configure interval for publish

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const questions = {
     message: 'What is the thing name?',
     validate: (given) => given && given.length > 0
   },
+  interval: {
+    type: 'input',
+    name: 'interval',
+    message: 'What interval do you want to publish on (milliseconds)?',
+    validate: (given) => given && given.length > 0
+  },
   broker: {
     type: 'input',
     name: 'broker',
@@ -20,16 +26,21 @@ const questions = {
 // Startup
 const program = new commander.Command();
 
+async function tick(){
+  console.log( chalk.yellow( `Tick [${Date.now()}].` ));
+}
+
 // Main handler
 async function main(){
   program
     .version('1.0.0')
     .option( '-b, --broker <broker>', 'DNS name for IoT broker' )
+    .option( '-i, --interval <interval>', 'Interval for message publish' )
     .option( '-t, --thing-name <thingName>', 'Unique thingname for AWS IoT Device Registry' );
 
   program.parse( process.argv );
 
-  let thingName = program.thingName;
+  let thingName = program.thingName || process.env.THING_NAME;
   if( !thingName) 
     thingName = (await inquirer.prompt([questions.thingName])).thingName;
 
@@ -37,12 +48,19 @@ async function main(){
   if( !broker )
     broker = (await inquirer.prompt([questions.broker])).broker;
 
+  let interval = program.interval || process.env.INTERVAL ;
+  if( !interval )
+    interval = (await inquirer.prompt([questions.interval])).interval;
+
   console.log( chalk.green( 'Starting up..' ));
   console.log( chalk.blue( 'Thing name: ', thingName ));  
-  console.log( chalk.blue( 'Broker Address: ', broker ));  
+  console.log( chalk.blue( 'Interval (milliseconds): ', interval ));  
+  console.log( chalk.blue( 'Broker Address: ', broker ));
+
+  setInterval( tick, interval );
 }
 
 // Entry point. Node does not (yet) support top-level await, so use promise
 main()
-  .then( () => console.log( chalk.dim.green( '..finished' )))
+  .then( () => console.log( chalk.green( '..Finished' )))
   .catch( err => console.error( chalk.red( 'Top level error:', err )));
